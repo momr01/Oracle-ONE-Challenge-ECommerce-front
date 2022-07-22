10859445ddb2c9f2033b903ec16c373ef30368ed
Maximiliano Montaña <maxi.omr01@gmail.com>
2022-07-22 18:59:05 -0300

    agregando carpeta helpers

---
+++ helpers/url.js
@@ -0,0 +1,8 @@
+const urlProductos =
+  "https://alura-ecommerce-jsonserver.herokuapp.com/productos";
+const urlCategorias =
+  "https://alura-ecommerce-jsonserver.herokuapp.com/categorias";
+const urlMensajes = "https://alura-ecommerce-jsonserver.herokuapp.com/mensajes";
+const urlUsuarios = "https://alura-ecommerce-jsonserver.herokuapp.com/usuarios";
+
+export { urlProductos, urlCategorias, urlMensajes, urlUsuarios };
